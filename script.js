fdc8f4e8107aaef2d2c19e2695d8e29978c0e530
gulsenaktas <aaktasgulsen@gmail.com>
2021-03-07 17:40:44 +0300

    1.1 enhancements

--- script.js
+++ script.js
@@ -65,72 +65,210 @@ const overlay=document.querySelector(".overlay")
         ease: Expo.easeInOut
     })
 
-    TweenMax.from(".box2", 1, {
+    TweenMax.from(".baloon-one", 1, {
         delay: 3.5,
         opacity: 0,
         ease: Expo.easeInOut
     })
 
-    TweenMax.from(".box6", 2, {
+    TweenMax.from(".baloon-two", 2, {
         delay: 4,
         opacity: 0,
         ease: Expo.easeInOut
     })
 
-    TweenMax.from(".box7", 2, {
-        delay: 3,
+    TweenMax.from(".baloon-three", 2, {
+        delay: 4.5,
         opacity: 0,
         ease: Expo.easeInOut
     })
 
-    TweenMax.from(".box8", 2, {
-        delay: 3,
+    TweenMax.from(".baloon-four", 2, {
+        delay: 5,
         opacity: 0,
         ease: Expo.easeInOut
     })
 
-    TweenMax.from(".box9", 2, {
+    TweenMax.from(".info-one", 1, {
         delay: 3.5,
         opacity: 0,
         ease: Expo.easeInOut
     })
 
-    TweenMax.from(".box5", 2, {
-        delay: 3,
+    TweenMax.from(".info-two", 2, {
+        delay: 4,
+        opacity: 0,
+        ease: Expo.easeInOut
+    })
+
+    TweenMax.from(".info-three", 2, {
+        delay: 4.5,
+        opacity: 0,
+        ease: Expo.easeInOut
+    })
+
+    TweenMax.from(".info-four", 2, {
+        delay: 5,
         opacity: 0,
         ease: Expo.easeInOut
     })
 
     TweenMax.from(".box7", 2, {
-        delay: 2,
+        delay: 2.4,
         opacity: 0,
         ease: Expo.linear
     })
 
-    
+    TweenMax.from(".box8", 2, {
+        delay: 2.5,
+        opacity: 0,
+        ease: Expo.easeInOut
+    })
 
-    
+    TweenMax.from(".box9", 2, {
+        delay: 2.5,
+        opacity: 0,
+        ease: Expo.easeInOut
+    })
+
+    TweenMax.from(".box5", 2, {
+        delay: 2.5,
+        opacity: 0,
+        ease: Expo.easeInOut
+    })
 // })
 
+const baloonOne = document.querySelector(".baloon-one");
+const baloonTwo = document.querySelector(".baloon-two");
+const baloonThree = document.querySelector(".baloon-three");
+const baloonFour = document.querySelector(".baloon-four");
+const infoOne = document.querySelector(".info-one");
+const infoTwo = document.querySelector(".info-two");
+const infoThree = document.querySelector(".info-three");
+const infoFour = document.querySelector(".info-four");
+const text1 = document.querySelector(".text1");
+const text2 = document.querySelector(".text2");
+const text3 = document.querySelector(".text3");
+const text4 = document.querySelector(".text4");
+const logo = document.querySelector(".logo");
+const hi = document.querySelector(".hi");
+const who = document.querySelector(".who");
+const note = document.querySelector(".note");
+const boxColor = document.querySelector(".boxcolor");
 
 
-// TweenMax.from(".", 1, {
-//     delay: 2,
-//     opacity: 0,
-//     ease: Expo.easeInOut
-//   })
 
-const boxs= document.querySelectorAll(".box")
-const rightContain= document.querySelector(".right-contain")
+infoOne.addEventListener("mouseenter", e=>{
+    
+        baloonOne.style.animation = "spin 3s linear 0s infinite"
+        baloonOne.style.borderColor = "#02cf54"
+        infoOne.style.background = "#ffffff"
+        text1.style.display= "block"
+        logo.style.fontFamily= "Nunito"
+        logo.style.paddingTop= "5vw"
+        hi.textContent = "Weather app"
+        hi.style.fontSize = "6vh"
+        who.textContent = "User techniques"
+        who.style.fontSize = "4vh"
+        note.innerText = "Local Storage\nAsync JS\nFetch API\nResposive Design\nSass"
+        note.style.fontSize = "3vh"
+        logo.style.backgroundColor ="#02cf54"
 
-
-boxs.forEach(box=>{
-    box.addEventListener("mouseenter", () => {
-        rightContain.classList.add("helper")
-    })    
 })
-// boxs.forEach(box=>{
-//     box.addEventListener("mouseleave", () => {
-//         rightContain.classList.remove("helper")
-//     })    
-// })
+
+infoOne.addEventListener("mouseleave", e=>{
+    baloonOne.style.animation = "None"
+    baloonOne.style.borderColor = "#989898"
+    infoOne.style.background= "url(./img/weather.png)"
+    infoOne.style.backgroundPosition = "center"
+    infoOne.style.backgroundSize = "55%"
+    infoOne.style.backgroundRepeat = "no-repeat"
+    text1.style.display= "none"
+})
+
+
+infoTwo.addEventListener("mouseenter", e=>{
+    
+        baloonTwo.style.animation = "spin 3s linear 0s infinite"
+        baloonTwo.style.borderColor = "#02cf54"
+        infoTwo.style.background = "#ffffff"
+        text2.style.display= "block"
+        logo.style.fontFamily= "Nunito"
+        logo.style.paddingTop= "5vw"
+        hi.textContent = "Exchange app"
+        hi.style.fontSize = "6vh"
+        who.textContent = "User techniques"
+        who.style.fontSize = "4vh"
+        note.innerText = "Async JS\nFetch API\nJSON Data"
+        note.style.fontSize = "3vh"
+        logo.style.backgroundColor ="#02cf54"
+
+})
+
+infoTwo.addEventListener("mouseleave", e=>{
+    baloonTwo.style.animation = "None"
+    baloonTwo.style.borderColor = "#989898"
+    infoTwo.style.background= "url(./img/exchange.png)"
+    infoTwo.style.backgroundPosition = "center"
+    infoTwo.style.backgroundSize = "55%"
+    infoTwo.style.backgroundRepeat = "no-repeat"
+    text2.style.display= "none"
+})
+
+
+infoThree.addEventListener("mouseenter", e=>{
+    
+        baloonThree.style.animation = "spin 3s linear 0s infinite"
+        baloonThree.style.borderColor = "#02cf54"
+        infoThree.style.background = "#ffffff"
+        text3.style.display= "block"
+        logo.style.fontFamily= "Nunito"
+        logo.style.paddingTop= "5vw"
+        hi.textContent = "Chat app"
+        hi.style.fontSize = "6vh"
+        who.textContent = "User techniques"
+        who.style.fontSize = "4vh"
+        note.innerText = "Object Oriented Programming\nReal-time Database\nLocal Storage\nDOM Manipulation\nResponsive Design"
+        note.style.fontSize = "3vh"
+        logo.style.backgroundColor ="#02cf54"
+
+})
+
+infoThree.addEventListener("mouseleave", e=>{
+    baloonThree.style.animation = "None"
+    baloonThree.style.borderColor = "#989898"
+    infoThree.style.background= "url(./img/stringifly.png)"
+    infoThree.style.backgroundPosition = "center"
+    infoThree.style.backgroundSize = "55%"
+    infoThree.style.backgroundRepeat = "no-repeat"
+    text3.style.display= "none"
+})
+
+
+infoFour.addEventListener("mouseenter", e=>{
+    
+        baloonFour.style.animation = "spin 3s linear 0s infinite"
+        baloonFour.style.borderColor = "#02cf54"
+        infoFour.style.background = "#ffffff"
+        text4.style.display= "block"
+        logo.style.fontFamily= "Nunito"
+        logo.style.paddingTop= "5vw"
+        hi.textContent = "Todo app"
+        hi.style.fontSize = "6vh"
+        who.textContent = "User techniques"
+        who.style.fontSize = "4vh"
+        note.innerText = "DOM Manipulation\nLocal Storage\nVanilla JavaScript"
+        note.style.fontSize = "3vh"
+        logo.style.backgroundColor ="#02cf54"
+
+})
+
+infoFour.addEventListener("mouseleave", e=>{
+    baloonFour.style.animation = "None"
+    baloonFour.style.borderColor = "#989898"
+    infoFour.style.background= "url(./img/todo.png)"
+    infoFour.style.backgroundPosition = "center"
+    infoFour.style.backgroundSize = "55%"
+    infoFour.style.backgroundRepeat = "no-repeat"
+    text4.style.display= "none"
+})
