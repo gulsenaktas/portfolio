7a60070e5168f7217dc19aef79e826ddd5e94355
gulsenaktas <aaktasgulsen@gmail.com>
2021-03-19 02:08:19 +0300

    2.0 Animation oncompleted functions added, Unnecessary css properties cleaned, Variable names reorganized

--- script.js
+++ script.js
@@ -1,144 +1,3 @@
-const overlay=document.querySelector(".overlay")
-
-// overlay.addEventListener("click", () => {
-    TweenMax.to(".overlay .h1", 2, {
-        opacity: 0,
-        y: -60,
-        ease: Expo.easeInOut
-    })
-    
-    TweenMax.to(".overlay span", 2, {
-        delay: .3,
-        opacity: 0,
-        y: -60,
-        ease: Expo.easeInOut
-    })
-    
-    TweenMax.to(".overlay", 2, {
-        delay: 1,
-        top: "-100%",
-        ease: Expo.easeInOut
-    })
-    TweenMax.from(".weather", 2, {
-        delay: 1.5,
-        opacity:0,
-        x: -60,
-        ease: Expo.easeInOut
-    })
-    TweenMax.from(".exchange", 3, {
-        delay: 1.5,
-        opacity:0,
-        x: -60,
-        ease: Expo.easeInOut
-    })
-    TweenMax.from(".stringfly", 4, {
-        delay: 1.5,
-        opacity:0,
-        x: -60,
-        ease: Expo.easeInOut
-    })
-    TweenMax.from(".todo", 5, {
-        delay: 1.5,
-        opacity:0,
-        x: -60,
-        ease: Expo.easeInOut
-    })
-    
-    TweenMax.from(".logo", 1, {
-        delay: 2,
-        opacity: 0,
-        y: -100,
-        ease: Expo.easeInOut
-    })
-    
-    
-    TweenMax.from(".contact", 2, {
-        delay: 2,
-        opacity: 0,
-        y: -100,
-        ease: Expo.easeInOut
-    })
-    
-    TweenMax.from(".text", 2, {
-        delay: 2,
-        opacity: 0,
-        y: -100,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".baloon-one", 1, {
-        delay: 3.5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".baloon-two", 2, {
-        delay: 4,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".baloon-three", 2, {
-        delay: 4.5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".baloon-four", 2, {
-        delay: 5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".info-one", 1, {
-        delay: 3.5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".info-two", 2, {
-        delay: 4,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".info-three", 2, {
-        delay: 4.5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".info-four", 2, {
-        delay: 5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".box7", 2, {
-        delay: 2.4,
-        opacity: 0,
-        ease: Expo.linear
-    })
-
-    TweenMax.from(".box8", 2, {
-        delay: 2.5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".box9", 2, {
-        delay: 2.5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-
-    TweenMax.from(".box5", 2, {
-        delay: 2.5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-// })
-
 const baloonOne = document.querySelector(".baloon-one");
 const baloonTwo = document.querySelector(".baloon-two");
 const baloonThree = document.querySelector(".baloon-three");
@@ -147,49 +6,42 @@ const infoOne = document.querySelector(".info-one");
 const infoTwo = document.querySelector(".info-two");
 const infoThree = document.querySelector(".info-three");
 const infoFour = document.querySelector(".info-four");
-const text1 = document.querySelector(".text1");
-const text2 = document.querySelector(".text2");
-const text3 = document.querySelector(".text3");
-const text4 = document.querySelector(".text4");
+const textOne = document.querySelector(".text-one");
+const textTwo = document.querySelector(".text-two");
+const textThree = document.querySelector(".text-three");
+const textFour = document.querySelector(".text-four");
 const logo = document.querySelector(".logo");
 const hi = document.querySelector(".hi");
 const who = document.querySelector(".who");
 const note = document.querySelector(".note");
 const boxColor = document.querySelector(".boxcolor");
-
+const logoTitle= document.querySelector(".logo-title");
 
 
 infoOne.addEventListener("mouseenter", e=>{
-    
-    baloonOne.style.animation = "spin 3s linear 0s infinite"
-    baloonOne.style.borderColor = "#02cf54"
-    infoOne.style.background = "#ffffff"
-    text1.style.display= "block"
 
-    gsap.to(".content",{
-        duration:1,
-        opacity: 1,
+    baloonOne.style.animation = "spin 3s linear 0s infinite"
+    baloonOne.style.borderColor = "#1ad3d3"
+    infoOne.style.background = "#ffffff"
+    textOne.style.display= "block"
+
+    logo.style.fontFamily= "Nunito"
+    logo.style.paddingTop= "15px"
+    logoTitle.style.paddingTop= "0px"
+    hi.textContent = "Weather app"
+    hi.style.fontSize = "30px"
+    who.textContent = "Used Techniques"
+    who.style.fontSize = "20px"
+    note.innerText = "Local Storage\nAsync JS\nFetch API\nResposive Design\nSass"
+    note.style.fontSize = "15px"
+
+    gsap.from(".content",{
+        duration: 1,
+        opacity:0,
+        y: -100,
         ease: Expo.easeInOut,
-        onComplete:complete
     })
 
-    function complete(){
-        logo.style.fontFamily= "Nunito"
-        logo.style.paddingTop= "5vw"
-        hi.textContent = "Weather app"
-        hi.style.fontSize = "6vh"
-        who.textContent = "Used Techniques"
-        who.style.fontSize = "4vh"
-        note.innerText = "Local Storage\nAsync JS\nFetch API\nResposive Design\nSass"
-        note.style.fontSize = "3vh"
-
-        gsap.from(".content",{
-            duration: 1,
-            y: -100,
-            opacity:0,
-            ease: Expo.easeInOut,
-        })
-    }
 })
 
 infoOne.addEventListener("mouseleave", e=>{
@@ -199,42 +51,33 @@ infoOne.addEventListener("mouseleave", e=>{
     infoOne.style.backgroundPosition = "center"
     infoOne.style.backgroundSize = "55%"
     infoOne.style.backgroundRepeat = "no-repeat"
-    text1.style.display= "none"
+    textOne.style.display= "none"
 })
 
 
 infoTwo.addEventListener("mouseenter", e=>{
-    
+
     baloonTwo.style.animation = "spin 3s linear 0s infinite"
-    baloonTwo.style.borderColor = "#02cf54"
+    baloonTwo.style.borderColor = "#1ad3d3"
     infoTwo.style.background = "#ffffff"
-    text2.style.display= "block"
+    textTwo.style.display= "block"
 
-    gsap.to(".content",{
+    logo.style.fontFamily= "Nunito"
+    logo.style.paddingTop= "15px"
+    logoTitle.style.paddingTop= "0px"
+    hi.textContent = "Exchange app"
+    hi.style.fontSize = "30px"
+    who.textContent = "Used Techniques"
+    who.style.fontSize = "20px"
+    note.innerText = "Async JS\nFetch API\nJSON Data"
+    note.style.fontSize = "15px"
+
+    gsap.from(".content",{
         duration:1,
-        opacity: 1,
+        y: -100,
+        opacity:0,
         ease: Expo.easeInOut,
-        onComplete:complete
     })
-
-    function complete(){
-        logo.style.fontFamily= "Nunito"
-        logo.style.paddingTop= "5vw"
-        hi.textContent = "Exchange app"
-        hi.style.fontSize = "6vh"
-        who.textContent = "Used Techniques"
-        who.style.fontSize = "4vh"
-        note.innerText = "Async JS\nFetch API\nJSON Data"
-        note.style.fontSize = "3vh"
-
-        gsap.from(".content",{
-            duration:1,
-            y: -100,
-            opacity:0,
-            ease: Expo.easeInOut,
-        })
-    }
-    
 
 })
 
@@ -245,43 +88,33 @@ infoTwo.addEventListener("mouseleave", e=>{
     infoTwo.style.backgroundPosition = "center"
     infoTwo.style.backgroundSize = "55%"
     infoTwo.style.backgroundRepeat = "no-repeat"
-    text2.style.display= "none"
+    textTwo.style.display= "none"
 })
 
 
 infoThree.addEventListener("mouseenter", e=>{
     
-        baloonThree.style.animation = "spin 3s linear 0s infinite"
-        baloonThree.style.borderColor = "#02cf54"
-        infoThree.style.background = "#ffffff"
-        text3.style.display= "block"
+    baloonThree.style.animation = "spin 3s linear 0s infinite"
+    baloonThree.style.borderColor = "#1ad3d3"
+    infoThree.style.background = "#ffffff"
+    textThree.style.display= "block"
 
+    logo.style.fontFamily= "Nunito"
+    logo.style.paddingTop= "15px"
+    logoTitle.style.paddingTop= "0px"
+    hi.textContent = "Chat app"
+    hi.style.fontSize = "30px"
+    who.textContent = "Used Techniques"
+    who.style.fontSize = "20px"
+    note.innerText = "Object Oriented Programming\nReal-time Database\nLocal Storage\nDOM Manipulation\nResponsive Design"
+    note.style.fontSize = "15px"
 
-        gsap.to(".content",{
-            duration:1,
-            opacity: 1,
-            ease: Expo.easeInOut,
-            onComplete:complete
-        })
-    
-        function complete(){
-            logo.style.fontFamily= "Nunito"
-            logo.style.paddingTop= "5vw"
-            hi.textContent = "Chat app"
-            hi.style.fontSize = "6vh"
-            who.textContent = "Used Techniques"
-            who.style.fontSize = "4vh"
-            note.innerText = "Object Oriented Programming\nReal-time Database\nLocal Storage\nDOM Manipulation\nResponsive Design"
-            note.style.fontSize = "3vh"
-    
-            gsap.from(".content",{
-                duration:1,
-                y: -100,
-                opacity:0,
-                ease: Expo.easeInOut,
-            })
-        }
-        
+    gsap.from(".content",{
+        duration:1,
+        y: -100,
+        opacity:0,
+        ease: Expo.easeInOut,
+    })
 
 })
 
@@ -292,41 +125,34 @@ infoThree.addEventListener("mouseleave", e=>{
     infoThree.style.backgroundPosition = "center"
     infoThree.style.backgroundSize = "55%"
     infoThree.style.backgroundRepeat = "no-repeat"
-    text3.style.display= "none"
+    textThree.style.display= "none"
 })
 
 
 infoFour.addEventListener("mouseenter", e=>{
     
-        baloonFour.style.animation = "spin 3s linear 0s infinite"
-        baloonFour.style.borderColor = "#02cf54"
-        infoFour.style.background = "#ffffff"
-        text4.style.display= "block"
+    baloonFour.style.animation = "spin 3s linear 0s infinite"
+    baloonFour.style.borderColor = "#1ad3d3"
+    infoFour.style.background = "#ffffff"
+    textFour.style.display= "block"
 
-        gsap.to(".content",{
-            duration:1,
-            opacity: 1,
-            ease: Expo.easeInOut,
-            onComplete:complete
-        })
+    logo.style.fontFamily= "Nunito"
+    logo.style.paddingTop= "15px"
+    logoTitle.style.paddingTop= "0px"
+    hi.textContent = "Todo app"
+    hi.style.fontSize = "30px"
+    who.textContent = "Used Techniques"
+    who.style.fontSize = "20px"
+    note.innerText = "DOM Manipulation\nLocal Storage\nVanilla JavaScript"
+    note.style.fontSize = "15px"
+
+    gsap.from(".content",{
+        duration:1,
+        y: -100,
+        opacity:0,
+        ease: Expo.easeInOut,
+    })
     
-        function complete(){
-            logo.style.fontFamily= "Nunito"
-            logo.style.paddingTop= "5vw"
-            hi.textContent = "Todo app"
-            hi.style.fontSize = "6vh"
-            who.textContent = "Used Techniques"
-            who.style.fontSize = "4vh"
-            note.innerText = "DOM Manipulation\nLocal Storage\nVanilla JavaScript"
-            note.style.fontSize = "3vh"
-        
-                gsap.from(".content",{
-                    duration:1,
-                    y: -100,
-                    opacity:0,
-                    ease: Expo.easeInOut,
-                })
-        }
 })
 
 infoFour.addEventListener("mouseleave", e=>{
@@ -336,5 +162,113 @@ infoFour.addEventListener("mouseleave", e=>{
     infoFour.style.backgroundPosition = "center"
     infoFour.style.backgroundSize = "55%"
     infoFour.style.backgroundRepeat = "no-repeat"
-    text4.style.display= "none"
+    textFour.style.display= "none"
+})
+
+function completed(info){
+    info.style.pointerEvents = "auto"
+}
+
+// ANIMATIONS
+
+// ---- OVERLAY ---- //
+gsap.to(".overlay .h1", 2, {
+    opacity: 0,
+    y: -60,
+    ease: Expo.easeInOut
+})
+
+gsap.to(".overlay span", 2, {
+    delay: .3,
+    opacity: 0,
+    y: -60,
+    ease: Expo.easeInOut
+})
+
+gsap.to(".overlay", 2, {
+    delay: 1,
+    top: "-100%",
+    ease: Expo.easeInOut
+})
+
+gsap.from(".logo", 1, {
+    delay: 2,
+    opacity: 0,
+    y: -100,
+    ease: Expo.easeInOut
+})
+
+
+gsap.from(".contact", 2, {
+    delay: 2,
+    opacity: 0,
+    y: -100,
+    ease: Expo.easeInOut
+})
+
+gsap.from(".text", 2, {
+    delay: 2,
+    opacity: 0,
+    y: -100,
+    ease: Expo.easeInOut
+})
+
+
+// ---- BALOONS ---- //
+gsap.from(".baloon-one, .info-one", 1, {
+    delay: 3.5,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    onComplete:completed,
+    onCompleteParams:[infoOne]
+})
+
+gsap.from(".baloon-two, .info-two", 2, {
+    delay: 4,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    onComplete:completed,
+    onCompleteParams:[infoTwo]
+
+})
+
+gsap.from(".baloon-three, .info-three", 2, {
+    delay: 4.5,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    onComplete:completed,
+    onCompleteParams:[infoThree]
+})
+
+gsap.from(".baloon-four, .info-four", 2, {
+    delay: 5,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    onComplete:completed,
+    onCompleteParams:[infoFour]
+})
+
+// ---- ROADS ---- //
+gsap.from(".box7", 2, {
+    delay: 2.4,
+    opacity: 0,
+    ease: Expo.linear
+})
+
+gsap.from(".box8", 2, {
+    delay: 2.5,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+gsap.from(".box9", 2, {
+    delay: 2.5,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+gsap.from(".box5", 2, {
+    delay: 2.5,
+    opacity: 0,
+    ease: Expo.easeInOut
 })
