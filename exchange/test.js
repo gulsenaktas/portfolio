b39a0052bfeba3ba5101d353feb03418438d1940
Hasan <parvifolia@Hasans-MacBook-Air.local>
2025-10-19 22:18:01 +0200

    3.0 weather app revision, bug fixes

--- exchange/test.js
+++ exchange/test.js
@@ -1,103 +1,122 @@
-const data={
-    USD: {EUR: 0.82, GBP: 0.74, TRY: 7.63},
-    EUR: {USD: 1.23, GBP: 0.91, TRY: 9.34},
-    GBP: {USD: 1.35, EUR: 1.10, TRY: 10.22},
-    TRY: {USD: 0.13, EUR: 0.11, GBP: 0.09},
-}
-
-const currencyKeys = Object.keys(data)
-
-function createCurrencyElements(element, root, inputName){
-    for (let i = 0; i < element.length; i++) {
-        
-        const currencyKeyDiv = document.createElement("div")
-        const currencyKeyInput = document.createElement("input")
-        
-        currencyKeyInput.setAttribute("type", "radio")
-        currencyKeyInput.setAttribute("name", inputName)
-        currencyKeyInput.setAttribute("id", inputName+element[i])
-        currencyKeyInput.setAttribute("value", element[i])
-
-        const currencyKeyLabel = document.createElement("label")
-        currencyKeyLabel.setAttribute("for", inputName + element[i])
-        currencyKeyLabel.textContent = element[i]
-
-        currencyKeyDiv.append(currencyKeyInput)
-        currencyKeyDiv.append(currencyKeyLabel)
-
-        root.append(currencyKeyDiv)
-    };
+const data = {
+  USD: {
+    EUR: 0.8654,
+    GBP: 0.6479,
+    TRY: 41.84,
+  },
+  EUR: {
+    USD: 1.1556,
+    GBP: 0.7479,
+    TRY: 48.64,
+  },
+  GBP: {
+    USD: 1.543,
+    EUR: 1.3367,
+    TRY: 55.9,
+  },
+  TRY: {
+    USD: 0.0239,
+    EUR: 0.0206,
+    GBP: 0.0179,
+  },
 };
+
+const currencyKeys = Object.keys(data);
+
+function createCurrencyElements(element, root, inputName) {
+  for (let i = 0; i < element.length; i++) {
+    const currencyKeyDiv = document.createElement("div");
+    const currencyKeyInput = document.createElement("input");
+
+    currencyKeyInput.setAttribute("type", "radio");
+    currencyKeyInput.setAttribute("name", inputName);
+    currencyKeyInput.setAttribute("id", inputName + element[i]);
+    currencyKeyInput.setAttribute("value", element[i]);
+
+    const currencyKeyLabel = document.createElement("label");
+    currencyKeyLabel.setAttribute("for", inputName + element[i]);
+    currencyKeyLabel.textContent = element[i];
+
+    currencyKeyDiv.append(currencyKeyInput);
+    currencyKeyDiv.append(currencyKeyLabel);
+
+    root.append(currencyKeyDiv);
+  }
+}
 
 const parentEl = document.querySelector("#currency-box-from");
 const fromInputName = "currency_from";
 createCurrencyElements(currencyKeys, parentEl, fromInputName);
 
-
 const parentToEl = document.querySelector("#currency-box-to");
 const toInputName = "currency_to";
 createCurrencyElements(currencyKeys, parentToEl, toInputName);
 
-
 const calculateButton = document.querySelector("#calculate-button");
-calculateButton.addEventListener("click", function(){
-    
-    const currencyResult = document.querySelector("#currency-result");
-    const fromTarget = document.querySelector("input[name='currency_from']:checked");
-    const toTarget = document.querySelector("input[name='currency_to']:checked");
+calculateButton.addEventListener("click", function () {
+  const currencyResult = document.querySelector("#currency-result");
+  const fromTarget = document.querySelector(
+    "input[name='currency_from']:checked"
+  );
+  const toTarget = document.querySelector("input[name='currency_to']:checked");
 
+  if (fromTarget == null || toTarget == null) {
+    currencyResult.innerHTML = '" choose currency! "';
+  } else {
+    const amount = document.querySelector("input[name='amount']").value;
+    const inputBorder = document.querySelector(".input-border");
 
-    if(fromTarget==null || toTarget==null){
-        currencyResult.innerHTML = '" choose currency! "'
+    const currentCurrencyObject = data[fromTarget.value];
+    const resultForOne = currentCurrencyObject[toTarget.value];
+    const result = (amount * resultForOne).toFixed(2);
 
-    }else {
-        const amount = document.querySelector("input[name='amount']").value
-        const inputBorder = document.querySelector(".input-border")
-        
-        
-        const currentCurrencyObject = data[fromTarget.value];
-        const resultForOne = currentCurrencyObject[toTarget.value];
-        const result = (amount * resultForOne).toFixed(2);
-
-
-        if(fromTarget.value==toTarget.value){
-            currencyResult.innerHTML = '" choose different currency! "' ;
-        }else{
-            if(isNaN(Number(amount))){
-                currencyResult.innerHTML = '" amount should be a number! "'
-                inputBorder.style= `border: 1px solid red;`
-            }else{
-                currencyResult.innerHTML = amount + " " + fromTarget.value + " = " + result + " " + toTarget.value;
-                inputBorder.style= `border: 3px solid  #3d3180;`
-            }
-        }
+    if (fromTarget.value == toTarget.value) {
+      currencyResult.innerHTML = '" choose different currency! "';
+    } else {
+      if (isNaN(Number(amount))) {
+        currencyResult.innerHTML = '" amount should be a number! "';
+        inputBorder.style = `border: 1px solid red;`;
+      } else {
+        currencyResult.innerHTML =
+          amount +
+          " " +
+          fromTarget.value +
+          " = " +
+          result +
+          " " +
+          toTarget.value;
+        inputBorder.style = `border: 3px solid  #3d3180;`;
+      }
     }
+  }
 });
 
-const getData = async function(currency){
-    const response =  await fetch(`https://api.exchangeratesapi.io/latest?base=${currency}`);
-    const responseJSON = await response.json();
+const getData = async function (currency) {
+  const response = await fetch(
+    `https://api.exchangeratesapi.io/latest?base=${currency}`
+  );
+  const responseJSON = await response.json();
 
-    return responseJSON
+  return responseJSON;
 };
 
-getData("USD").then(e=>{
-    data.USD.EUR = e.rates.EUR
-    data.USD.GBP = e.rates.GBP
-    data.USD.TRY = e.rates.TRY
+getData("USD").then((e) => {
+  data.USD.EUR = e.rates.EUR;
+  data.USD.GBP = e.rates.GBP;
+  data.USD.TRY = e.rates.TRY;
 });
-getData("EUR").then(e=>{
-    data.EUR.USD = e.rates.USD
-    data.EUR.GBP = e.rates.GBP
-    data.EUR.TRY = e.rates.TRY
+getData("EUR").then((e) => {
+  data.EUR.USD = e.rates.USD;
+  data.EUR.GBP = e.rates.GBP;
+  data.EUR.TRY = e.rates.TRY;
 });
-getData("GBP").then(e=>{
-    data.GBP.EUR = e.rates.EUR
-    data.GBP.USD = e.rates.USD
-    data.GBP.TRY = e.rates.TRY
+getData("GBP").then((e) => {
+  data.GBP.EUR = e.rates.EUR;
+  data.GBP.USD = e.rates.USD;
+  data.GBP.TRY = e.rates.TRY;
 });
-getData("TRY").then(e=>{
-    data.TRY.EUR = e.rates.EUR
-    data.TRY.GBP = e.rates.GBP
-    data.TRY.USD = e.rates.USD
+getData("TRY").then((e) => {
+  data.TRY.EUR = e.rates.EUR;
+  data.TRY.GBP = e.rates.GBP;
+  data.TRY.USD = e.rates.USD;
 });
--- weather/scripts/forecast.js
+++ weather/scripts/forecast.js
@@ -1,21 +1,29 @@
-const key ="hqObQwwS2fQpHXqlAgFGGWZrlDnQCyS1"
+const key = "zpka_17523a0fa5fe4164a8c4d991be0fc58e_ae9c986e";
 
 const getWeather = async (id) => {
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/"
-    const query = `${id}?apikey=${key}`
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const url = `${base}${id}`;
 
-    const response = await fetch(base + query);
-    const data = await response.json()
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${key}`
+        }
+    });
 
-    return data[0]
+    const data = await response.json();
+    return data[0];
 }
 
 const getCity = async (city) => {
     const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-    const query = `?apikey= ${key}&q=${city}`;
+    const url = `${base}?q=${encodeURIComponent(city)}`;
 
-    const response = await fetch(base + query);
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${key}`
+        }
+    });
+
     const data = await response.json();
-
-    return data[0]
+    return data[0];
 }
